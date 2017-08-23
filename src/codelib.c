0912fa0358137917512a1d8b407319908355796b
Lkchan <lkchan0719@gmail.com>
2017-08-23 22:45:59 +0800

    add dependencies

---
+++ src/codelib.c
@@ -0,0 +1,16 @@
+
+//
+// codelib.c
+//
+// Copyright (c) 2017 JesseChen <lkchan0719@gmail.com>
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+
+}
