f905629a9fd14d696262bdb99428837bed6adde7
Lkchan <lkchan0719@gmail.com>
2017-08-25 07:47:58 +0800

    fix indent

--- src/codelib.c
+++ src/codelib.c
@@ -49,18 +49,18 @@ int main(int argc, char **argv)
 {
 	command_t cmd;
 	command_init(&cmd, argv[0],CODE_LIB_VERSION);
-	command_option(&cmd, 
-			       "-o","--out <dir>",
-				   "change the default output directory [deps]",
-				   setopt_dir);
+	command_option(&cmd,	
+			"-o","--out <dir>",
+			"change the default output directory [deps]",
+			setopt_dir);
 	command_option(&cmd,
-			       "-s","--search <key>",
-				   "search package name or descriptions contain the keyword",
-				   setopt_searchkey);
+			"-s","--search <key>",
+			"search package name or descriptions contain the keyword",
+			setopt_searchkey);
 	command_option(&cmd,
-			      "-l", "--local",
-				  "install package defined in package.json dependencies",
-				  setopt_local);
+			"-l", "--local",
+			"install package defined in package.json dependencies",
+			setopt_local);
 	command_parse(&cmd,argc,argv);
 
 	cleanup();
